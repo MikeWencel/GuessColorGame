6217895a8a2e3ad3f1c08e15af8dcc128cc3830a
MikeWencel <wencel.michael@gmail.com>
2018-09-21 14:37:00 +0200

    vol. 1

---
+++ js/script.js
@@ -0,0 +1,76 @@
+var colors = guessColors(6);
+var squares = document.querySelectorAll('.square');
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("display");
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+var resetButton = document.querySelector("#reset");
+
+
+resetButton.addEventListener("click", function() {
+    colors = guessColors(6);
+
+    pickedColor = pickColor();
+
+    colorDisplay.textContent = pickedColor;
+    
+    for (var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i]
+    }
+
+    h1.style.backgroundColor = '#232323';
+});
+
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length ; i++) {
+    //Dodaje kolory dla kwadratów
+    squares[i].style.backgroundColor = colors[i]
+
+    //Działanie pointera - klikanie
+    squares[i].addEventListener('click', function () {
+    //Pobieranie klikniętego koloru
+    var clickedColor = this.style.backgroundColor;
+    if (clickedColor === pickedColor){
+        messageDisplay.textContent = 'Prawidłowo!';
+        changeColors(clickedColor);
+        h1.style.backgroundColor = clickedColor;
+        resetButton.textContent = "zagraj jeszcze raz"
+    }else {
+        this.style.backgroundColor ='#232323';
+        messageDisplay.textContent = 'Próbuj dalej'
+    }
+
+    })
+}
+
+function changeColors(color) {
+    for (var i = 0; i < squares.length; i++) {
+      squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+var random = Math.floor(Math.random() * colors.length)
+    return colors[random]
+}
+
+function guessColors(num) {
+    //Tworzymy array
+    var arr = []
+    //
+    for (var i = 0; i < num;i++){
+    arr.push(randomColor())
+    }
+    // Zwracamy wylosowany arr
+    return arr;
+}
+
+
+function randomColor() {
+   var a = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var c = Math.floor(Math.random() * 256);
+    return 'rgb(' + a +', ' + b +', ' + c+')';
+}
